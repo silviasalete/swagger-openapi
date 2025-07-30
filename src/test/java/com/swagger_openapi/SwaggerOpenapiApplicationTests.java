77eb4fce2854affa84ab9a1eaf7f138a709103ee
Silvia Gomes <silvia.gomes@extreme.digital>
2025-07-30 14:12:20 -0300

    build: first commit

---
+++ src/test/java/com/swagger_openapi/SwaggerOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swagger_openapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerOpenapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
